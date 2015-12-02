c7d41ee3057b6aaa7721be9ae86dfa3e9decd426
Brendan Germain <brendan.germain@nasdaqomx.com>
2015-12-02 15:10:13 -0500

    use next to skip iteration

--- recipes/data_bag.rb
+++ recipes/data_bag.rb
@@ -4,14 +4,14 @@ if node['users']
   node['users'].each do |username|
     user = data_bag_item('users', username)
 
-    if user['git_user'] && user['git_user']['enabled']
-      git_user user['id'] do
-        home user['home']
-        full_name user['git_user']['full_name']
-        email user['git_user']['email']
-        private_key user['git_user']['private_key']
-        known_hosts user['git_user']['known_hosts']
-      end
+    next if user['git_user'].nil? || !user['git_user']['enabled']
+
+    git_user user['id'] do
+      home user['home']
+      full_name user['git_user']['full_name']
+      email user['git_user']['email']
+      private_key user['git_user']['private_key']
+      known_hosts user['git_user']['known_hosts']
     end
   end
 end
