7d6f17c1ed86de17a888d16f937fcdf2b6ad4f6c
Brendan Germain <brendan.germain@nasdaqomx.com>
2015-12-02 14:49:58 -0500

    allow gitconfig to be set via attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,14 @@
 # -*- mode: ruby; coding: utf-8; -*-
 
 default['git_user']['include_git_recipe'] = true
+default['git_user']['gitconfig']['alias'] = {
+  'co' => 'checkout',
+  'ci' => 'commit',
+  'rb' => 'rebase',
+  'st' => 'status'
+}
+default['git_user']['gitconfig']['color'] = {
+  'status' => 'auto',
+  'branch' => 'auto',
+  'ui' => 'auto'
+}
